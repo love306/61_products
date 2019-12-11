12f93e87c22b3d9f99d6d39cf18bf510b87a46ec
love306 <j127289j@gmail.com>
2019-12-11 20:53:10 +0800

    add files check

--- 61_products.py
+++ 61_products.py
@@ -1,12 +1,19 @@
-#讀取檔案
+#檢查檔案是否存在
+import os
 products = []
-with open ('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name,price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	#讀取檔案
+	print('hi,find files')
+	with open ('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name,price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('There is no files')
+
 #輸入檔案
 while True:
 	name = input('請輸入名稱:')
